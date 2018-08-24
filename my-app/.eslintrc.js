8da39bebda23ab95c31151d4071b3d2ca58c6d69
barco <b3181401@gmail.com>
2018-08-24 17:59:16 +0300

    add: final form of linter

--- my-app/.eslintrc.js
+++ my-app/.eslintrc.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const prettierOptions = JSON.parse(fs.readFileSync('./.prettierrc', 'utf8'));
+
 module.exports = {
   env: {
     browser: true,
@@ -19,15 +22,15 @@ module.exports = {
     },
     sourceType: 'module',
   },
-  plugins: ['babel', 'react'],
+  plugins: ['babel', 'react', 'prettier'],
   rules: {
-    indent: ['error', 4],
+    'prettier/prettier': ['error', prettierOptions],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'react/jsx-boolean-value': 0,
     'react/jsx-closing-bracket-location': 1,
-    'react/jsx-curly-spacing': [2, 'always'],
     'react/jsx-indent-props': [1, 2],
     'react/jsx-no-undef': 1,
     'react/jsx-uses-react': 1,
@@ -35,5 +38,6 @@ module.exports = {
     'react/react-in-jsx-scope': 1,
     'react/prefer-es6-class': 1,
     'react/jsx-no-bind': 1,
+    'no-console': 'warn',
   },
 };
--- my-app/src/registerServiceWorker.js
+++ my-app/src/registerServiceWorker.js
@@ -19,8 +19,10 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
+  // eslint-disable-next-line no-undef
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    // eslint-disable-next-line no-undef
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
@@ -30,6 +32,7 @@ export default function register() {
     }
 
     window.addEventListener('load', () => {
+      // eslint-disable-next-line no-undef
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
